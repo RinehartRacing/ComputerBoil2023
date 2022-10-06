8934b32225d30b23a303c7a11da78faa3e8eb416
Rusty Rinehart <rustyrinehart@email.arizona.edu>
2022-10-05 22:35:39 -0700

    Created animated thermometer and a python file for testing GUI functionality

--- gui.py
+++ gui.py
@@ -18,6 +18,10 @@ class GUI:
 
     def __init__(self, master):
         self.master = master
+
+        # Attributes
+        self.temperature = None
+
         self.master.title("Let's Boil a Computer Again")
         self.temp_display()
         self.graph_display()
@@ -27,21 +31,58 @@ class GUI:
         self.settings_display()
         self.master.grid_columnconfigure(0, weight=1)
         self.master.grid_columnconfigure(1, weight=10)
-        self.master.grid_rowconfigure(0, weight= 10)
+        self.master.grid_rowconfigure(0, weight=10)
 
     def temp_display(self):
         temp_label = ttk.Label(self.master, text="Temperature:")
         temp_label.grid(row=0, column=0)
-        temp_value = ttk.Label(self.master, text="NULL")
-        temp_value.grid(row=1, column=0)
-        canvas = Canvas(self.master, width=100, height=100)
-        canvas.create_rectangle(40, 10, 60, 80, fill="white", width=0)
-        canvas.create_oval(35, 70, 65, 100, fill="red", width=0)
-        canvas.grid(row=2, column=0)
-        # self.temp_image = PhotoImage(file="thermometer.png").subsample(4, 4)
-        # image_label = ttk.Label(self.master, image=self.temp_image)
-        # image_label.grid(row=2, column=0)
+        self.temp_value = ttk.Label(self.master, text="NULL")
+        self.temp_value.grid(row=1, column=0)
+        self.therm_canvas = Canvas(self.master, width=100, height=100)
+        self.therm_canvas.create_rectangle(
+            40, 10, 60, 80, fill="white", width=0)
+        self.therm_circ = self.therm_canvas.create_oval(
+            35, 70, 65, 100, fill="black", width=0)
+        # Create rectangle that will change size
+        self.therm_rect = self.therm_canvas.create_rectangle(
+            40, 70, 60, 80, fill="black", width=0)
+        self.therm_canvas.grid(row=2, column=0)
 
+    def set_temperature(self, new_temp):
+        """Sets the temperature and adjusts thermometer animation accordingly"""
+        self.temperature = new_temp
+        # Temperatures only visually supported between 20 and 60
+        if new_temp < 20:
+            new_temp = 20
+        if new_temp > 60:
+            new_temp = 60
+        # Adjust temperature value on GUI
+        self.temp_value.config(text=self.temperature)
+        # Calculate height needed by rectangle to simulate where thermometer is
+        height = (new_temp - 20) * 1.5
+        # Update rectangle coordinates to change height
+        self.therm_canvas.coords(self.therm_rect, 40, 70 - height, 60, 80)
+        # Get a scaled color shift value based on temperature
+        # Get red value in hex
+        color_shift = int(((new_temp - 20) / 40.0) * 255)
+        red = hex(color_shift)[2:]
+        # If statement needed to give leading zero to single digit hex value
+        if len(red) == 1:
+            red = "0" + red
+        # Green value won't change
+        green = "00"
+        # Get blue value in hex
+        blue = hex(255 - color_shift)[2:]
+        # If statement needed to give leading zero to single digit hex value
+        if len(blue) == 1:
+            blue = "0" + blue
+        # Generate hex color
+        new_color = f"#{red}{green}{blue}"
+        print(new_color)
+        print(f"Red = {color_shift}, Blue = {255 - color_shift}")
+        # Update color
+        self.therm_canvas.itemconfig(self.therm_rect, fill=new_color)
+        self.therm_canvas.itemconfig(self.therm_circ, fill=new_color)
 
     def graph_display(self):
         graph_label = ttk.Label(self.master, text="Graph Here")
---
+++ gui_test.py
@@ -0,0 +1,37 @@
+"""
+    Engineering 498
+    Let's Boil a Computer
+    Rusty Rinehart
+    Chris Bremser
+    Jesus Arias
+    Prasanna Raut
+    Sean Brown
+
+    This file tests the functionality of the GUI via software
+"""
+from tkinter import Tk
+from gui import GUI
+
+
+def test_temperature(gui):
+    while True:
+        for i in range(20, 60):
+            print(f"Temperature = {i}")
+            gui.set_temperature(i)
+            # Input waits for user to hit enter in terminal so we can visualize each changing temperature
+            input()
+        for i in range(60, 20, -1):
+            print(f"Temperature = {i}")
+            gui.set_temperature(i)
+            input()
+
+
+def main():
+    root = Tk()
+    gui = GUI(root)
+    test_temperature(gui)
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
